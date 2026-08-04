cdf504def54db9e816babde78d9abaa39a0175f7
Utkarsh Agarwal <162401935+utkarshagarwal03@users.noreply.github.com>
2026-08-05 02:38:53 +0530

    Time: 4 ms (73.92%), Space: 45.6 MB (11.1%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         HashMap<Integer,Integer> hm=new HashMap<>();
+        Stack<Integer> s=new Stack<>();
         for(int i=0;i<nums2.length;i++){
             hm.put(nums2[i],-1);
-            for(int j=i+1;j<nums2.length;j++){
-                if(nums2[i]<nums2[j]){
-                    hm.put(nums2[i],nums2[j]);
-                    break;
-                }
+            while(!s.isEmpty() && s.peek()<nums2[i]){
+                hm.put(s.peek(),nums2[i]);
+                s.pop();
             }
+            s.push(nums2[i]);
     }
         for(int i=0;i<nums1.length;i++){
             nums1[i]=hm.get(nums1[i]);
